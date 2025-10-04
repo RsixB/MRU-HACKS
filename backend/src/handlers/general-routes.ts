ae624e97a102c4c3ea65d0e4bce9aacba560b7ee
luc <elyktrar@gmail.com>
2025-10-04 16:12:03 -0600

    gen route start

---
+++ backend/src/handlers/general-routes.ts
@@ -0,0 +1,35 @@
+import { MessageModel } from "../database/models/Messages.ts";
+import { UserModel } from "../database/models/Users.ts";
+import type { Request, Response, NextFunction} from "express-serve-static-core";
+
+
+export const addFriend = async (req: Request, res: Response) => {
+}
+
+export const acceptRequest = async (req: Request, res: Response) => {
+}
+
+export const rejectRequest = async (req: Request, res: Response) => {}
+
+
+export const getPendingFriendRequest = async (req: Request, res: Response) => {
+
+}
+
+
+export const sendMessage = async (req: Request, res: Response) => {
+
+}
+
+
+export const getMessages = async (req: Request, res: Response) => {
+
+}
+
+export const searchUser = async (req: Request, res: Response) => {
+
+}
+
+export const getFriends = async (req: Request, res: Response) => {
+
+}
--- backend/src/routes/general-routes.ts
+++ backend/src/routes/general-routes.ts
@@ -1,4 +1,10 @@
 import { Router } from "express";
+import { getFriends, addFriend, rejectRequest, acceptRequest } from "../handlers/general-routes.ts"; 
 
 const router = Router()
+//NEEDS AUTH
+router.post("/get-friends")
+router.post("/add-friend")
+router.post("/accept-request")
+router.post("/reject-request")
 export default router
